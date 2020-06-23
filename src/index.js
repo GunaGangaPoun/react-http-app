507c31d991b30cf5a070cf0d93b435a351d75a54
GunaGangaPoun <64207940+GunaGangaPoun@users.noreply.github.com>
2020-06-23 13:49:20 +0530

    Setting Default Global configuration using axis
    url is splited as BAseUrl and specific
    
    default headers added globally

--- src/index.js
+++ src/index.js
@@ -5,21 +5,25 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
-    axios.interceptors.request.use( request => {
-        //can edit request config
-        console.log(request);
-        return request;
-    }, error => {
-        console.log(error);
-        return Promise.reject(error);
-    });
-    axios.interceptors.response.use( response => {
-        console.log(response);
-        return response;
-    }, error => {
-        console.log(error);
-        return Promise.reject(error);
-    });
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorisation'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use( request => {
+    //can edit request config
+    console.log(request)
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+axios.interceptors.response.use( response => {
+    console.log(response);
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
     
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
