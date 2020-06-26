297d0842425b8b7cbd3c7d909eccc93dfab51f47
GunaGangaPoun <64207940+GunaGangaPoun@users.noreply.github.com>
2020-06-26 13:08:22 +0530

    Preparing the project for Routing
    insatlled react router
    Modified the folder structure for Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,51 +1,12 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from '../Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId : null,
-        error: false
-    }
-    componentDidMount () {
-        axios.get('/posts')
-        .then ((response) => {
-            const posts = response.data.slice(0,4);
-            const updatedPosts = posts.map ((post) => {
-                    return {
-                        ...post,
-                        author:'GG'
-                    }
-            });
-            this.setState ({ posts : updatedPosts});
-           // console.log (response);
-        }).catch((error) => {
-           this.setState({error : true});
-        });
-    }
-    postSelectedHandler = (id) => {
-        this.setState ({selectedPostId : id});
-    }
     render () {
-        let posts = <p>Something went wrong!</p>;
-        if(!this.state.error) {
-            posts = this.state.posts.map((post) => {
-                return <Post 
-                            key={post.id} 
-                            title={post.title} 
-                            author={post.author} 
-                            clicked={() => this.postSelectedHandler (post.id)}/>
-            });
-        }
         return (
             <div className="Blog">
-                <header>
+              <header>
                     <nav>
                         <ul>
                             <li><a href="/">Home</a></li>
@@ -53,18 +14,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                 {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Posts />
             </div>
         );
     }
-}
+}  
 
 export default Blog;
---
+++ src/containers/Posts/Posts.js
@@ -0,0 +1,50 @@
+import React,{ Component } from 'react';
+import Post from '../../components/Post/Post';
+import axios from '../../axios';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount () {
+        axios.get('/posts')
+        .then ((response) => {
+            console.log('ha');
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map ((post) => {
+                    return {
+                        ...post,
+                        author:'GG'
+                    }
+            });
+            this.setState ({ posts : updatedPosts});
+           // console.log (response);
+        }).catch((error) => {
+          // this.setState({error : true});
+        });
+    }
+    postSelectedHandler = (id) => {
+      //  this.setState ({selectedPostId : id});
+    }
+    render () {
+        let posts = <p>Something went wrong!</p>;
+      //  if(!this.state.error) {
+            posts = this.state.posts.map((post) => {
+                return <Post 
+                            key={post.id} 
+                            title={post.title} 
+                            author={post.author} 
+                            clicked={() => this.postSelectedHandler (post.id)}/>
+            });
+     //   }
+    
+        return(
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+
+export default Posts;
