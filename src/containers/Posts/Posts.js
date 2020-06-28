37cb5411745244ce8e95f996480143255232774c
GunaGangaPoun <64207940+GunaGangaPoun@users.noreply.github.com>
2020-06-28 17:14:49 +0530

    navigating posts programatically
    navigate post via route props history instead adding a Link wrapper to  the component.

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -2,7 +2,7 @@ import React,{ Component } from 'react';
 import Post from '../../components/Post/Post';
 import './Posts.css';
 import axios from '../../axios';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 class Posts extends Component {
     state = {
@@ -25,19 +25,23 @@ class Posts extends Component {
         });
     }
     postSelectedHandler = (id) => {
-       this.setState ({selectedPostId : id});
+      // this.setState ({selectedPostId : id});
+        //this.props.history.push('/'+id);
+        this.props.history.push({ pathname : '/' + id });
     }
     render () {
         let posts = <p>Something went wrong!</p>;
       //  if(!this.state.error) {
             posts = this.state.posts.map((post) => {
                 return (
-                    <Link to={'/'+post.id} key={post.id}>
-                        <Post 
+                   // <Link to={'/'+post.id} key={post.id}>
+                        <Post
+                            key={post.id} 
                             title={post.title} 
                             author={post.author} 
                             clicked={() => this.postSelectedHandler ( post.id )}/>
-                    </Link>)
+                   // </Link>
+                   )
             });
      //   }
     
